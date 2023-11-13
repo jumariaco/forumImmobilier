f000c9029dd05be6da38ae7946b317ce0b305dcf
jumariaco <justinemariacourt@hotmail.fr>
2023-11-13 13:37:23 +0100

    Création de commentaires reliés à une publication

--- src/Controller/PublicationController.php
+++ src/Controller/PublicationController.php
@@ -3,9 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Publication;
+use App\Entity\Commentaire;
 use App\Form\PublicationType;
+use App\Form\CommentaireType;
 use App\Repository\PublicationRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,6 +19,11 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/publication')]
 class PublicationController extends AbstractController
 {
+    private $entityManager;
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
     #[Route('/', name: 'app_publication_index', methods: ['GET'])]
     public function index(PublicationRepository $publicationRepository): Response
     {
@@ -42,11 +52,79 @@ class PublicationController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_publication_show', methods: ['GET'])]
-    public function show(Publication $publication): Response
+    // #[Route('/{id}', name: 'app_publication_show', methods: ['GET', 'POST'])]
+    // public function show(Publication $publication, Request $request): Response
+    // {
+
+    //     // partie Commentaires
+    //     // On crée l'objet "Commentaire" vierge
+    //     $commentaire = new Commentaire();
+
+    //     // on génère le formulaire
+    //     $commentaireForm = $this->createForm(CommentaireType::class, $commentaire);
+    //     $commentaireForm->handleRequest($request);
+
+    //     // Traitement du formulaire
+    //     if ($commentaireForm->isSubmitted() && $commentaireForm->isValid()) {
+    //         $commentaire->setCreatedAt(new \DateTime());
+    //         $commentaire->setPublication($publication);
+
+    //         // On récupère le contenu du champ parentid
+    //         $parentid = $commentaireForm->get('parentid')->getData();
+
+    //         // On ajoute l'utilisateur connecté à l'entité Commentaire
+    //         $commentaire->setUser($this->getUser());
+
+    //         // On va chercher le commentaire correspondant
+    //         $entityManager = $this->getDoctrine()->getManager();
+
+    //         if($parentid != null) {
+    //             $parent = $entityManager->getRepository(Commentaire::class)->find($parentid);
+    //             // on définit le parent
+    //             $commentaire->setParent($parent);
+    //         } else {
+    //             $commentaire->setParent(null);
+    //         }
+            
+    //         $this->entityManager->persist($commentaire);
+    //         $this->entityManager->flush();
+
+    //         $this->addFlash('success', 'Commentaire ajouté');
+    //         return $this->redirectToRoute('app_publication_show', ['id' => $publication->getId()]);
+    //         // return $this->redirectToRoute('publication', ['slug' => $publication->getSlug()]);
+    //     }
+
+
+    //     return $this->render('publication/show.html.twig', [
+    //         'publication' => $publication,
+    //         'commentaireForm' => $commentaireForm->createView(),
+    //     ]);
+    // }
+
+    #[Route('/{id}', name: 'app_publication_show', methods: ['GET', 'POST'])]
+    public function show(Publication $publication, Request $request, EntityManagerInterface $em): Response
     {
+        $commentaire = new Commentaire();
+        $commentaire->setPublication($publication);
+        if($this->getUser()){
+            $commentaire->setUser($this->getUser());
+        }
+        $form = $this->createForm(CommentaireType::class, $commentaire);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($commentaire);
+            $em->flush();
+
+            $this->addFlash('success', 'Votre commentaire a bien été ajouté');
+           
+            return $this->redirectToRoute('app_publication_show', ['id' => $publication->getId()]);
+        }
+
+
         return $this->render('publication/show.html.twig', [
             'publication' => $publication,
+            'form' => $form->createView(),
         ]);
     }
 
--- src/Form/CommentaireType.php
+++ src/Form/CommentaireType.php
@@ -8,23 +8,43 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 Use Vich\UploaderBundle\Form\Type\VichImageType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
 
 class CommentaireType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('contenu', CKEditorType::class, ['purify_html' => true])
-            ->add('choixRetenu')
+            // ->add('user', CheckboxType::class, [
+            //     'mapped' => false
+            // ])
+            ->add('contenu', CKEditorType::class, [
+                'label' => 'Poster un nouveau commentaire : ',
+                'purify_html' => true])
+            ->add('choixRetenu', HiddenType::class, [
+                'mapped' => false
+            ])
             ->add('imageFile', VichImageType::class, [
                 'required' => false,
                 'allow_delete' => true,
-                'label' => 'Image Publication',
+                'label' => 'Insérer une image :',
             ])
-            ->add('publication')
-            ->add('user')
+            // ->add('publication', EntityType::class, [
+            //     'class' => Publication::class,
+            //     'choice_label' => 'titre',
+            //     'placeholder' => 'Sélectionnez la publication'
+            // ])
+            
+            ->add('parentid', HiddenType::class, [
+                'mapped' => false
+            ])
+            ->add ('envoyer', SubmitType::class)
         ;
     }
 
