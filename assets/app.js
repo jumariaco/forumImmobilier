d7cf6467097aa99fee7858fa7b4ed32a83447888
jumariaco <justinemariacourt@hotmail.fr>
2023-11-19 23:08:47 +0100

    ajout footer + slogan home et articles

--- assets/app.js
+++ assets/app.js
@@ -10,7 +10,7 @@
 import * as bootstrap from 'bootstrap';
 import './styles/app.scss';
 
-
+// -------------------------EMETTRE UN COMMENTAIRE-------------------------
 // on met un écouteur d'évènements sur tous les boutons "répondre" (à un commentaire)
 document.querySelectorAll('[data-reply]').forEach((element) => {
     element.addEventListener('click', function() {
@@ -30,4 +30,60 @@ document.querySelectorAll('[data-replies]').forEach((element) => {
     });
 });
 
+// --------------------------FIN DE L'EMETTEUR DE COMMENTAIRES--------------------------
 
+// -------------------------HOME-------------------------
+// function([string1, string2],target id,[color1,color2])    
+consoleText(['Bienvenue sur le 1er forum d\'expertise immobilière'], 'text',['primary','secondary']);
+
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = ['#fff'];
+  var visible = true;
+  var con = document.getElementById('console');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id)
+  target.setAttribute('style', 'color:' + colors[0])
+  window.setInterval(function() {
+
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount)
+      window.setTimeout(function() {
+        var usedColor = colors.shift();
+        colors.push(usedColor);
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute('style', 'color:' + colors[0])
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function() {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount)
+      letterCount += x;
+    }
+  }, 120)
+  window.setInterval(function() {
+    if (visible === true) {
+      con.className = 'console-underscore hidden'
+      visible = false;
+
+    } else {
+      con.className = 'console-underscore'
+
+      visible = true;
+    }
+  }, 400)
+}
+
+
+// -------------------------FIN HOME-------------------------
